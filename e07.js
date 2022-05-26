f071326ef5dc6fcefe51a74184cc28799bb34bf1
AlexsanderNeves <poopove.dnt@gmail.com>
2022-05-26 03:30:24 -0300

    Initial commit

---
+++ e07.js
@@ -0,0 +1,16 @@
+//CONDIÇÃO SIMPLES
+var vel = 70
+console.log(`A velocidade do seu caroo é ${vel}Km/h`)
+if (vel > 60){ 
+console.log('Voce ultrapassou o limite de velocidade permitido. MULTADO ')
+}
+console.log('Dirija sempre usando cinto de segurança')
+
+//CONDIÇÃO COMPOSTA!
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
